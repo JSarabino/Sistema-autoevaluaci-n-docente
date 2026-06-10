dbd328191c3c5a26be8bbd35f0f1b3ae4847dd3f
JSarabino <jsarabino@unicauca.edu.co>
2026-06-10 14:56:26 -0500

    chore: remove generated files and add backend env example

--- sistema-autoevaluacion-docentes-main/backend/dist/routes/autenticacion.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const autenticacion_1 = require("../controllers/autenticacion");
-const router = (0, express_1.Router)();
-router.post('/login', autenticacion_1.login);
-exports.default = router;
